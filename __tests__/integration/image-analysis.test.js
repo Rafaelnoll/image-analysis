9a617a6322ae0a26a79c348354a5c3fe97a4389e
Rafael Noll <rafanoll2004@gmail.com>
2026-01-28 14:49:51 -0300

    Chore: Adding jest configurations

---
+++ __tests__/integration/image-analysis.test.js
@@ -0,0 +1,7 @@
+const { describe, it, expect } = require("@jest/globals");
+
+describe("Image analyser test suite", () => {
+  it.todo("Should analyse successfuly the image returning the results");
+  it.todo("Should return status code 400 when given an empty queryString");
+  it.todo("Should return status code 500 when given an invalid image URL");
+});
